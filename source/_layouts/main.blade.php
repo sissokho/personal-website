9d648e267a6a9039861408b84f5341832286cb3f
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-05-21 13:28:23 +0000

    Add fonts

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ $page->language ?? 'en' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="canonical" href="{{ $page->getUrl() }}">
-        <meta name="description" content="{{ $page->description }}">
-        <title>{{ $page->title }}</title>
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-        <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
-    </head>
-    <body class="text-gray-900 font-sans antialiased">
-        @yield('body')
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="canonical" href="{{ $page->getUrl() }}">
+    <meta name="description" content="{{ $page->description }}">
+    <title>{{ $page->title }} - Moustapha Sissokho</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Play:wght@400;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+</head>
+
+<body class="font-montserrat antialiased">
+    @yield('body')
+</body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     content: ["source/**/*.blade.php", "source/**/*.md", "source/**/*.html"],
     theme: {
+        fontFamily: {
+            montserrat: ["Montserrat", "sans-serif"],
+            play: ["Play", "serif"],
+        },
         extend: {},
     },
     plugins: [],
