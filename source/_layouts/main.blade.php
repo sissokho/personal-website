9d60fa5c1d177980a31e88b146df81eb12587432
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-06-08 16:03:59 +0000

    Change fonts

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -9,9 +9,7 @@
     <title>{{ $page->title }} - Moustapha Sissokho</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Play:wght@400;700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Play:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
 </head>
