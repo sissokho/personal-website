b5bf64532698453a075be32877df2cc850a210a3
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-08-01 14:46:23 +0000

    Fix infinite loop build
    When running the npm watch command, the building process is locked in an
    infinite loop. It is apparently due to the fact that webpack doesn’t
    support the glob option when registering PostCSS dependencies.
    
    In order to fix it we can tell webpack to only watch specific files or
    in my case use the fast-glob npm package.
    
    See this link for a better explanation of the problem:
    https://tailwindcss.com/docs/content-configuration#styles-rebuild-in-an-infinite-loop
    
    Read article about how to solve this with fast-glob: https://tips.dev/tailwind-css/how-to-fix-laravel-mix-and-tailwind-infinite-loop/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-    content: ["source/**/*.blade.php", "source/**/*.md", "source/**/*.html"],
+    // content: ["source/**/*.blade.php", "source/**/*.md", "source/**/*.html"],
+    // content: [
+    //     "./source/index.blade.php",
+    //     "./source/contact.blade.php",
+    //     "./source/work.blade.php",
+    //     "./source/_components/**/*.blade.php",
+    //     "./source/_layouts/**/*.blade.php",
+    //     "./source/_partials/**/*.blade.php}",
+    //     "./source/_projects/*.md",
+    // ],
+    content: require("fast-glob").sync("./source/**/*{blade.php,md,html}"),
     theme: {
         fontFamily: {
             montserrat: ["Montserrat", "sans-serif"],
