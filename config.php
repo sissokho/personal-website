90e8a73de2747ad65587965aabbaa1bd899adc7c
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-08-01 10:57:58 +0000

    Update site description

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'production' => false,
     'baseUrl' => '',
     'title' => 'Mouhamadou Moustapha SISSOKHO',
-    'description' => 'My personal website',
+    'description' => 'Web Developer, Laravel - Livewire - Alpine.js - Tailwind CSS - Jigsaw.',
     'github' => 'https://github.com/sissokho',
     'twitter' => 'https://twitter.com/mm_sissokho',
     'linkedin' => 'https://www.linkedin.com/in/mouhamadou-moustapha-sissokho-548a55125/',
