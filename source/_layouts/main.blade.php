b2c2a64cdfb210ffad93bfc7aed5b758d2d21185
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-08-01 18:22:41 +0000

    Increase horizontal padding for larger screens

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -6,17 +6,17 @@
 </head>
 
 <body class="min-h-screen flex flex-col bg-white font-monserrat antialiased tracking-wide relative">
-    <header class="p-5 border-b border-slate-200">
+    <header class="p-5 border-b border-slate-200 sm:px-10 md:px-16">
         <div class="flex items-center max-w-7xl mx-auto">
             <x-navigation :page="$page" />
         </div>
     </header>
 
-    <main class="py-14 px-5 flex-1">
+    <main class="py-14 px-5 flex-1 sm:px-10 md:px-16">
         @yield('body')
     </main>
 
-    <footer class="p-5 bg-cyan-900 text-slate-200">
+    <footer class="p-5 bg-cyan-900 text-slate-200 sm:px-10 md:px-16">
         <div class="max-w-7xl mx-auto">
             <div class="flex flex-col gap-3 sm:flex-row sm:justify-between sm:items-center">
                 <p class="text-center" x-data>
@@ -25,8 +25,7 @@
                 <!-- Github profile -->
                 <ul class="flex justify-center gap-5">
                     <li>
-                        <a href="{{ $page->github }}" target="_blank" class="group"
-                            aria-label="View my Github profile">
+                        <a href="{{ $page->github }}" target="_blank" class="group" aria-label="View my Github profile">
                             <x-icons.github aria-hidden="true" class="w-8 h-8 text-slate-300 group-hover:text-white" />
                         </a>
                     </li>
@@ -36,10 +35,8 @@
                         </a>
                     </li>
                     <li>
-                        <a href="{{ $page->linkedin }}" target="_blank" class="group"
-                            aria-label="View my Linkedin profile">
-                            <x-icons.linkedin aria-hidden="true"
-                                class="w-8 h-8 text-slate-300 group-hover:text-white" />
+                        <a href="{{ $page->linkedin }}" target="_blank" class="group" aria-label="View my Linkedin profile">
+                            <x-icons.linkedin aria-hidden="true" class="w-8 h-8 text-slate-300 group-hover:text-white" />
                         </a>
                     </li>
                 </ul>
@@ -47,16 +44,13 @@
             <div class="mt-2">
                 <p class="text-center text-sm">
                     Built with
-                    <a href="https://jigsaw.tighten.com/" target="_blank"
-                        class="underline decoration-dotted underline-offset-4">
+                    <a href="https://jigsaw.tighten.com/" target="_blank" class="underline decoration-dotted underline-offset-4">
                         Jigsaw
                     </a>,
-                    <a href="https://tailwindcss.com/" target="_blank"
-                        class="underline decoration-dotted underline-offset-4">
+                    <a href="https://tailwindcss.com/" target="_blank" class="underline decoration-dotted underline-offset-4">
                         Tailwind CSS
                     </a>
-                    & <a href="https://alpinejs.dev/" target="_blank"
-                        class="underline decoration-dotted underline-offset-4">
+                    & <a href="https://alpinejs.dev/" target="_blank" class="underline decoration-dotted underline-offset-4">
                         Alpine.js
                     </a>
                 </p>
