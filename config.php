458e0ceeccff66ecc975bd7499daaadb9ee031b7
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2023-01-22 11:18:18 +0000

    update linkedin profile link

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ return [
     'description' => 'Web Developer, Laravel - Livewire - Alpine.js - Tailwind CSS - Jigsaw.',
     'github' => 'https://github.com/sissokho',
     'twitter' => 'https://twitter.com/mm_sissokho',
-    'linkedin' => 'https://www.linkedin.com/in/mouhamadou-moustapha-sissokho-548a55125/',
+    'linkedin' => 'https://www.linkedin.com/in/sissokho',
     'collections' => [
         'projects' => [
             'path' => 'work/{slug}',
