f42f7cf2ebae62b5bf3889a1af5c76f331e85e5b
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2023-01-22 12:24:58 +0000

    Change Project link cta design

--- source/_components/project/cta.blade.php
+++ source/_components/project/cta.blade.php
@@ -1,4 +1,3 @@
-<a {{ $attributes }}
-    class="text-lg border-2 border-cyan-700 text-cyan-700 py-1 px-4 rounded-full hover:bg-cyan-700 hover:text-white">
+<a {{ $attributes }} class="text-lg bg-cyan-700 text-white py-1 px-4 rounded-full hover:bg-cyan-600">
     {{ $slot }}
 </a>
