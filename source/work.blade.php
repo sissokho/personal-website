18d6aa0c3b5041fec6f347a6960c047bb4cb88bc
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2023-01-22 11:08:17 +0000

    Remove learning projects

--- source/work.blade.php
+++ source/work.blade.php
@@ -3,31 +3,22 @@
 @section('title', 'My work')
 
 @section('body')
-    <div class="max-w-7xl mx-auto">
-        <h1 class="text-3xl text-center font-bold tracking-wide md:text-left">
-            <span class="inline-block bg-pink-600 text-white px-2 py-1"
-                style="border-radius: 91% 9% 90% 10% / 29% 82% 18% 71%">
-                Selected Work
-            </span>
-        </h1>
-        <p class="text-lg text-slate-700 font-bold text-center mt-2 md:text-left">Here are few projects I have worked on.</p>
+<div class="max-w-7xl mx-auto">
+    <h1 class="text-3xl text-center font-bold tracking-wide md:text-left">
+        <span class="inline-block bg-pink-600 text-white px-2 py-1"
+            style="border-radius: 91% 9% 90% 10% / 29% 82% 18% 71%">
+            Selected Work
+        </span>
+    </h1>
+    <p class="text-lg text-slate-700 font-bold text-center mt-2 md:text-left">Here are few projects I have worked on.
+    </p>
 
-        <section class="mt-28">
-            <x-project.category>Main projects</x-project.category>
-            <ul class="mt-10 space-y-20">
-                @foreach ($projects->filter->ofType('main') as $project)
-                    <x-project.row :project="$project" />
-                @endforeach
-            </ul>
-        </section>
-
-        <section class="mt-28">
-            <x-project.category>Learning & Practice</x-project.category>
-            <ul class="mt-10 space-y-20">
-                @foreach ($projects->filter->ofType('practice') as $project)
-                    <x-project.row :project="$project" />
-                @endforeach
-            </ul>
-        </section>
-    </div>
+    <section class="mt-28">
+        <ul class="mt-10 space-y-20">
+            @foreach ($projects->filter->ofType('main') as $project)
+            <x-project.row :project="$project" />
+            @endforeach
+        </ul>
+    </section>
+</div>
 @endsection
