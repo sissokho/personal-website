3bf568d5587d0d1f7f9a6ee35428b3b632598fc7
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2023-01-22 12:41:28 +0000

    use tag component

--- source/_layouts/project.blade.php
+++ source/_layouts/project.blade.php
@@ -11,14 +11,13 @@
     <!-- Technologies used -->
     <div class="flex gap-2 justify-center flex-wrap mt-5 md:justify-start">
         @foreach ($page->tags as $tag)
-        <span class="bg-cyan-100/80 text-sm font-normal text-cyan-900 tracking-wider py-1 px-4 rounded-full">
-            {{ $tag }}
-        </span>
+        <x-project.tag>{{ $tag }}</x-project.tag>
         @endforeach
     </div>
 
     <!-- Content -->
-    <article class="markdown mt-16 prose prose-slate md:prose-lg prose-a:text-cyan-700 prose-a:underline prose-a:decoration-cyan-600 prose-a:decoration-2 prose-a:underline-offset-4 hover:prose-a:text-cyan-900 hover:prose-a:decoration-cyan-800 prose-img:max-w-full prose-img:h-auto prose-img:block prose-img:mx-auto prose-img:shadow-md prose-img:shadow-slate-400 prose-img:rounded max-w-none">
+    <article
+        class="markdown mt-16 prose prose-slate md:prose-lg prose-a:text-cyan-700 prose-a:underline prose-a:decoration-cyan-600 prose-a:decoration-2 prose-a:underline-offset-4 hover:prose-a:text-cyan-900 hover:prose-a:decoration-cyan-800 prose-img:max-w-full prose-img:h-auto prose-img:block prose-img:mx-auto prose-img:shadow-md prose-img:shadow-slate-400 prose-img:rounded max-w-none">
         {!! (new TightenCo\Jigsaw\Parsers\MarkdownParser())->parse($page->getContent()) !!}
     </article>
 </div>
